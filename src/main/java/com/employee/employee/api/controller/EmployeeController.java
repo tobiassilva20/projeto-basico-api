8a16c066e6d057a2851e8c0c4087b4f0abe908f3
Tobias Silva <tobiassilva@hotmail.it>
2023-03-24 23:20:23 -0300

    Métodos foram comentados

--- src/main/java/com/employee/employee/api/controller/EmployeeController.java
+++ src/main/java/com/employee/employee/api/controller/EmployeeController.java
@@ -11,26 +11,46 @@ import java.util.ArrayList;
 public class EmployeeController {
     @Autowired
     EmployeeServiceImpl employeeService;
+
+    /**
+     * Handles the root request
+     * @return
+     */
     @GetMapping("/")
     public String runRoot(){
         return "Wellcome to my Rest API\n";
     }
 
+    /**
+     * Find all employees
+     * @return
+     */
     @GetMapping("/findall")
     public ArrayList<Employee> findAll(){
         return employeeService.findAll();
     }
 
+    /**
+     * Find an Employee by an id.
+     * @param id
+     * @return
+     */
     @GetMapping("/find/{id}")
     public Employee findById(@PathVariable Long id){
         return employeeService.findAllEmployeeById(id);
     }
 
+    /**
+     * Save all employees in database
+     */
     @PostMapping("/saveall")
     public void saveAll(){
         employeeService.saveAll();
     }
 
+    /**
+     * Delete all data from database
+     */
     @DeleteMapping("/deleteall")
     public void deleteAll(){
         employeeService.deleteAll();
